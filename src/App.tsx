b937fe9641f9bcc868d9fb93f01700a0d54d922f
Bordercolin <cwillems1903@gmail.com>
2024-11-19 14:34:04 +0100

    added fonts

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Missions from "./pages/Missions";
 import Home from "./pages/Home";
 
-
 import "./App.css";
-
 
 export default function App() {
   return (
     <BrowserRouter>
+      <link
+        href="https://api.fontshare.com/v2/css?f[]=khand@400&f[]=array@400&display=swap"
+        rel="stylesheet"
+      ></link>
       <Routes>
         <Route>
           <Route index element={<Home />} />
           <Route path="/missions" element={<Missions />} />
-          
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(<App />);
