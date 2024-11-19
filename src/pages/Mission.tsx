e09ff1b0eaf81e00ca9cbe2fe4dfc38b41b4e869
Bordercolin <cwillems1903@gmail.com>
2024-11-19 14:50:11 +0100

    added extra styling

--- src/pages/Mission.tsx
+++ src/pages/Mission.tsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom';
 
+
 export default function Mission() {
     const params = useParams();
   return (
--- src/pages/Missions.tsx
+++ src/pages/Missions.tsx
@@ -1,8 +1,10 @@
 import Quest from "../components/Quest";
+import styles from "../css/missions.module.css";
 
 export default function Missions() {
   return (
-    <div className="App">
+    <div className={styles.App}>
+      <img src="/images/missionsbackground.jpg" alt="Space background" />
       <Quest />
     </div>
   );
